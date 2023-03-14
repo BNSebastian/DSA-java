c2ca1a17312444c5a4cc7675195b308064baf339
Sebastian BN <105242756+BNSebastian@users.noreply.github.com>
2023-03-14 21:36:04 +0200

    update

--- src/main/java/numeric/leetcode/ReverseInteger.java
+++ src/main/java/numeric/leetcode/ReverseInteger.java
@@ -36,21 +36,4 @@ public class ReverseInteger {
         return (int)reversedNumber;
     }
 
-    public int reverse2(int x) {
-        long finalNum = 0;
-        while (x != 0) {
-            int lastDig = x % 10;
-            finalNum += lastDig;
-            finalNum = finalNum * 10;
-            x /= 10;
-        }
-        finalNum = finalNum / 10;
-        if (finalNum > Integer.MAX_VALUE || finalNum<Integer.MIN_VALUE) {
-            return 0;
-        }
-        if(x < 0){
-            return (int)(-1 * finalNum);
-        }
-        return (int)finalNum;
-    }
 }
